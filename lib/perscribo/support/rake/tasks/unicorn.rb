9af55eefdf4aec87182483c2338e03913d711dfb
Adam Eberlin <aeberlin@enova.com>
2014-10-31 11:03:02 -0500

    Bug fix.

--- lib/perscribo/support/rake/tasks/unicorn.rb
+++ lib/perscribo/support/rake/tasks/unicorn.rb
@@ -1,26 +1,28 @@
 require 'perscribo/guard'
 
-module Perscribo
-  module Support
-    module Rake
-      class UnicornTasks
-        include ::Rake::DSL if defined? ::Rake::DSL
+if defined? ::Rake::DSL
+  module Perscribo
+    module Support
+      module Rake
+        class UnicornTasks
+          include ::Rake::DSL
 
-        CONFIG_TEMPLATE = [
-          "require 'perscribo/unicorn/configurator'",
-          "::Perscribo::Unicorn::Configurator.capture!(self)"
-        ].join("\n")
+          CONFIG_TEMPLATE = [
+            "require 'perscribo/unicorn/configurator'",
+            "::Perscribo::Unicorn::Configurator.capture!(self)"
+          ].join("\n")
 
-        def install_tasks
-          namespace :perscribo do
-            namespace :unicorn do
-              desc 'Create default config/unicorn.rb hook for logging'
-              task :hook_config do
-                Dir.mkdir('config') unless Dir.exists?('config')
-                File.open('config/unicorn.rb', 'a') do |f|
-                  f.write("\n#{CONFIG_TEMPLATE}\n")
+          def install_tasks
+            namespace :perscribo do
+              namespace :unicorn do
+                desc 'Create default config/unicorn.rb hook for logging'
+                task :hook_config do
+                  Dir.mkdir('config') unless Dir.exists?('config')
+                  File.open('config/unicorn.rb', 'a') do |f|
+                    f.write("\n#{CONFIG_TEMPLATE}\n")
+                  end
+                  puts "Installed, require config/unicorn.rb to capture logging!"
                 end
-                puts "Installed, require config/unicorn.rb to capture logging!"
               end
             end
           end
@@ -28,6 +30,6 @@ module Perscribo
       end
     end
   end
-end
 
-::Perscribo::Support::Rake::UnicornTasks.new.install_tasks
+  ::Perscribo::Support::Rake::UnicornTasks.new.install_tasks
+end
